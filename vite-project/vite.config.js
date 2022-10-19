e5554186bdeba3d3f58575dd008e0a62bb006954
isac holstad <wisac22@gmail.com>
2022-10-19 21:25:22 +0200

    project turned to a tailwind and added a product html

---
+++ vite-project/vite.config.js
@@ -0,0 +1,15 @@
+// vite.config.js
+import {resolve} from 'path'
+import {defineConfig} from 'vite'
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                home: resolve(__dirname, 'index.html'),
+                product_page : resolve(__dirname, 'productPage.html')
+
+            }
+        }
+    }
+});
