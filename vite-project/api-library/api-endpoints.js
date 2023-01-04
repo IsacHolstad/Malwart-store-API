cfb28257131d76a2250e047303d6b50ff3b276b1
isac holstad <wisac22@gmail.com>
2023-01-04 13:18:33 +0100

    endpoint for export default

--- vite-project/api-library/api-endpoints.js
+++ vite-project/api-library/api-endpoints.js
@@ -2,4 +2,5 @@ const USER_SIGN_UP = 'https://fakestoreapi.com/users';
 const USER_LOG_IN = 'https://fakestoreapi.com/auth/login';
 const ALL_USERS = 'https://fakestoreapi.com/users';
 
-export { USER_SIGN_UP, USER_LOG_IN, ALL_USERS };
+export default {USER_SIGN_UP, USER_LOG_IN, ALL_USERS};
+
